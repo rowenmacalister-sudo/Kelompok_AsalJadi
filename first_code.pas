53cc69d49b1d18a9258d42698ca18255573934f0
ferdinand_macalister <rowenmacalister@gmail.com>
2025-12-16 21:26:32 +0700

    feat: add global variables for accounts and histories

--- first_code.pas
+++ first_code.pas
@@ -13,6 +13,10 @@ type
         nominal : longint;
         waktu : string;
     end;
+var
+    acc : array[1..3] of account;
+    his : array[1..100] of history;
+    t_acc : integer = 0;
 
 
 begin

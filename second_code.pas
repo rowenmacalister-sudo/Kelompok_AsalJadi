736ef9a07c8667c2ba9ad9d672807600de3800cf
Billiam Agsa Tatema Hia <billiamhia@gmail.com>
2025-12-18 19:23:06 +0700

    Refactor history handling in account records
    Signed-off-by: Billiam Agsa Tatema Hia <billiamhia@gmail.com>

--- second_code.pas
+++ second_code.pas
@@ -4,22 +4,23 @@ interface
 uses SysUtils;
 
 type
-    account = record
-        username : string;
-        PIN : string;
-        balance : longint;
-        history_c : integer;
-    end;
-
     history = record
         deskripsi : string;
         nominal : longint;
         waktu : string;
     end;
 
+    account = record
+        username : string;
+        PIN : string;
+        balance : longint;
+        history_c : integer;
+        his : array[1..100] of history;
+    end;
+
+
 var
     acc : array[1..3] of account;
-    his : array[1..100] of history;
     t_acc : integer;
 
 procedure addhis(i:integer; des:string; nominal:longint);
@@ -34,22 +35,32 @@ uses crt;
 procedure addhis(i:integer; des:string; nominal:longint);
 begin
     inc(acc[i].history_c);
-    his[acc[i].history_c].deskripsi := des;
-    his[acc[i].history_c].nominal := nominal;
-    his[acc[i].history_c].waktu := DateTimeToStr(Now);
+    acc[i].his[acc[i].history_c].deskripsi := des;
+    acc[i].his[acc[i].history_c].nominal := nominal;
+    acc[i].his[acc[i].history_c].waktu := DateTimeToStr(Now);
 end;
 
 procedure showhis(i:integer);
 var b:integer;
 begin
     clrscr;
-    writeln('=== RIWAYAT ===');
+    writeln('=== RIWAYAT ', acc[i].username, ' ===');
+
     if acc[i].history_c = 0 then
     begin
         writeln('Belum ada transaksi.');
         readln;
         exit;
     end;
+
+    for b := 1 to acc[i].history_c do
+    begin
+        writeln(b,'. ', acc[i].his[b].deskripsi,
+                ' Rp', acc[i].his[b].nominal);
+        writeln('   ', acc[i].his[b].waktu);
+    end;
+    readln;
+end;
 
     for b := 1 to acc[i].history_c do
     begin

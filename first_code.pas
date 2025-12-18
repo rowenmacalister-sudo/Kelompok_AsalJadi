8c6226223f1a12d1eb204f8a9c5c2bfc6b6fc689
ferdinand_macalister <rowenmacalister@gmail.com>
2025-12-18 12:24:20 +0700

    feat: register new account
    Signed-off-by: ferdinand_macalister <rowenmacalister@gmail.com>

--- first_code.pas
+++ first_code.pas
@@ -74,5 +74,26 @@ begin
     end;
 end;
 
+procedure regis;
+begin
+    if t_acc = 3 then
+    begin
+        writeln('Maksimum 3 akun.');
+        readln;
+        exit;
+    end;
+
+    inc(t_acc);
+    clrscr;
+    write('Masukkan username: '); readln(acc[t_acc].username);
+    write('Masukkan PIN: '); readln(acc[t_acc].PIN);
+
+    acc[t_acc].balance := 0;
+    acc[t_acc].history_c := 0;
+
+    writeln('Akun berhasil dibuat!');
+    readln;
+end;
+
 begin
 end.

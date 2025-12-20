215b88a45497bed09cfc55c519dfb5d28e35eaf1
ferdinand_macalister <rowenmacalister@gmail.com>
2025-12-20 22:30:36 +0700

    Update first_code.pas
    Signed-off-by: ferdinand_macalister <rowenmacalister@gmail.com>

--- first_code.pas
+++ first_code.pas
@@ -1,30 +1,7 @@
 program Mbanking;
 uses crt, SysUtils, second_code;
 
-procedure regis;
-begin
-    if t_acc = 3 then
-    begin
-        writeln('Maksimum 3 akun.');
-        readln;
-        exit;
-    end;
 
-    inc(t_acc);
-    clrscr;
-
-    write('Masukkan username: ');
-    readln(acc[t_acc].username);
-
-    write('Masukkan PIN: ');
-    readln(acc[t_acc].PIN);
-
-    acc[t_acc].balance := 0;
-    acc[t_acc].history_c := 0;
-
-    writeln('Akun berhasil dibuat!');
-    readln;
-end;
 
 procedure login(i:integer);
 var pin:string;
